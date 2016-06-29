7b6a05ee3713c8ff04381604b06c05b88528c4fb
Julian V. Modesto <julian.modesto@nytimes.com>
2016-06-29 15:04:18 -0400

    Check MD5 and size first

--- S3toGS.go
+++ S3toGS.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"io"
@@ -9,6 +10,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -19,6 +21,8 @@ import (
 
 	"golang.org/x/net/context"
 	"google.golang.org/cloud/storage"
+
+	"github.com/pivotal-golang/bytefmt"
 )
 
 var (
@@ -28,6 +32,7 @@ var (
 	localDir     = flag.String("localDir", "", "local directory")
 	gcpProjectId = flag.String("gcpProjectId", "", "gcp project id")
 	gsBucket     = flag.String("gsBucket", "", "gs bucket")
+	dryRun       = flag.Bool("dryRun", false, "dry run")
 )
 
 type Exit struct{ Code int }
@@ -41,6 +46,7 @@ func handleExit() {
 		}
 		panic(e) // not an Exit, bubble up
 	}
+	os.Exit(0)
 }
 
 // timing
@@ -48,6 +54,32 @@ func handleExit() {
 func timeTrack(start time.Time, name string) {
 	elapsed := time.Since(start)
 	log.Printf("%s took %s", name, elapsed)
+}
+
+func writeToGS(file *os.File, w *storage.Writer) error {
+	var content io.Reader
+	content, err := os.Open(file.Name())
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	const maxSlurp = 1 << 20
+	var buf bytes.Buffer
+	n, err := io.CopyN(&buf, content, maxSlurp)
+	if err != nil && err != io.EOF {
+		log.Fatalf("Error reading from stdin: %v, %v", n, err)
+		return err
+	}
+	w.ContentType = http.DetectContentType(buf.Bytes())
+	_, err = io.Copy(w, io.MultiReader(&buf, content))
+	if cerr := w.Close(); cerr != nil && err == nil {
+		err = cerr
+	}
+	if err != nil {
+		log.Fatalf("Write error: %v", err)
+		return err
+	}
+	return nil
 }
 
 func main() {
@@ -74,7 +106,7 @@ func main() {
 	defer gsClient.Close()
 
 	// S3 List
-	s3List, err := s3Client.ListObjects(&s3.ListObjectsInput{
+	s3List, err := s3Client.ListObjectsV2(&s3.ListObjectsV2Input{
 		Bucket: aws.String(*s3Bucket),
 		Prefix: aws.String(*s3Prefix),
 	})
@@ -83,61 +115,64 @@ func main() {
 		panic(Exit{1})
 	}
 
+	amtTransferred := uint64(0)
+
 	for _, key := range s3List.Contents {
+		gsAttrs, gsErr := gsClient.Bucket(*gsBucket).Object(*key.Key).Attrs(gcpContext)
+
+		s3MD5 := strings.Replace(*key.ETag, "\"", "", -1)
+		s3Size := *key.Size
+
 		localFilepath := filepath.Join(*localDir, *key.Key)
 
-		// Create local file path and file
-		err := os.MkdirAll(filepath.Dir(localFilepath), 0777)
-		if err != nil {
-			log.Fatal("Failed to create dirs", err)
-			panic(Exit{1})
-		}
-		file, err := os.Create(localFilepath)
-		if err != nil {
-			log.Fatal("Failed to create file", err)
-			panic(Exit{1})
-		}
-		defer file.Close()
+		if gsErr != nil || // doesn't exist in GS
+			!strings.EqualFold(s3MD5, hex.EncodeToString(gsAttrs.MD5)) ||
+			s3Size != gsAttrs.Size {
 
-		// Download from S3
-		fmt.Println("Downloading", *key.Key, "from S3 to", localFilepath)
-		s3Downloader.Download(file,
-			&s3.GetObjectInput{
-				Bucket: aws.String(*s3Bucket),
-				Key:    aws.String(*key.Key),
-			})
+			if gsErr == nil && strings.EqualFold(s3MD5, hex.EncodeToString(gsAttrs.MD5)) {
+				fmt.Println("Hash matches, skipping", *key.Key)
+			} else if gsErr == nil && s3Size == gsAttrs.Size {
+				fmt.Println("Size matches, skipping", *key.Key)
+			} else if *dryRun {
+				fmt.Println("Would download/upload", *key.Key)
+			} else {
+				amtTransferred += uint64(s3Size)
 
-		// Upload to GS
-		// https://github.com/golang/build/blob/master/cmd/upload/upload.go
-		fmt.Println("Uploading to GS at", *key.Key)
-		w := gsClient.Bucket(*gsBucket).Object(*key.Key).NewWriter(gcpContext)
-		var content io.Reader
-		content, err = os.Open(file.Name())
-		if err != nil {
-			log.Fatal(err)
-			panic(Exit{1})
-		}
-		const maxSlurp = 1 << 20
-		var buf bytes.Buffer
-		n, err := io.CopyN(&buf, content, maxSlurp)
-		if err != nil && err != io.EOF {
-			log.Fatalf("Error reading from stdin: %v, %v", n, err)
-			panic(Exit{1})
-		}
-		w.ContentType = http.DetectContentType(buf.Bytes())
-		_, err = io.Copy(w, io.MultiReader(&buf, content))
-		if cerr := w.Close(); cerr != nil && err == nil {
-			err = cerr
-		}
-		if err != nil {
-			log.Fatalf("Write error: %v", err)
-			panic(Exit{1})
-		}
+				// Create local file path and file
+				err := os.MkdirAll(filepath.Dir(localFilepath), 0777)
+				if err != nil {
+					log.Fatal("Failed to create dirs", err)
+					panic(Exit{1})
+				}
+				file, err := os.Create(localFilepath)
+				if err != nil {
+					log.Fatal("Failed to create file", err)
+					panic(Exit{1})
+				}
+				defer file.Close()
 
-		// Delete local file
-		fmt.Println("Removing", file.Name())
-		os.Remove(file.Name())
+				// Download from S3
+				fmt.Println("Downloading from S3", *key.Key, "to", localFilepath)
+				s3Downloader.Download(file,
+					&s3.GetObjectInput{
+						Bucket: aws.String(*s3Bucket),
+						Key:    aws.String(*key.Key),
+					})
+
+				// Upload to GS
+				// https://github.com/golang/build/blob/master/cmd/upload/upload.go
+				fmt.Println("Uploading", localFilepath, "to GS at", *key.Key)
+				w := gsClient.Bucket(*gsBucket).Object(*key.Key).NewWriter(gcpContext)
+				writeToGS(file, w)
+
+				// Delete local file
+				fmt.Println("Removing", file.Name())
+				os.Remove(file.Name())
+			}
+		} else {
+			fmt.Println("Already in GS, skipping", *key.Key)
+		}
 	}
 
-	os.Exit(0)
+	fmt.Println("Amount transferred", bytefmt.ByteSize(amtTransferred))
 }
